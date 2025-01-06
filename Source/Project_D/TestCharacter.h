f84394f4fd22b06fa7e227db2a45523806df361b
loenna <arterycutter@gmail.com>
2025-01-06 17:56:25 +0900

    특정 bone 별로 damage를 입으면 사지가 절단되는 코드 구현 중

--- Source/Project_D/TestCharacter.h
+++ Source/Project_D/TestCharacter.h
@@ -4,12 +4,48 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "UObject/ObjectRename.h"
 #include "TestCharacter.generated.h"
+
+UENUM(BlueprintType)
+enum class EMovementState : uint8
+{
+	NORMAL,
+	CLAWING
+};
 
 UCLASS()
 class PROJECT_D_API ATestCharacter : public ACharacter
 {
 	GENERATED_BODY()
+
+	bool bDoOnce = false;
+	bool bIsGateOpen = false;
+	
+	FTimerHandle OnceTimerHandle;
+	FTimerHandle DelayTimerHandle;
+
+protected:
+	UPROPERTY()
+	int32 AppliedDamage;
+
+	UPROPERTY()
+	FName HitBoneName;
+
+	UPROPERTY()
+	class AActor* DamageCauser;
+
+	UPROPERTY()
+	TMap<FName, int32> BoneDurability;
+
+	UPROPERTY()
+	TArray<FName> BrokenBones;
+
+	UPROPERTY()
+	bool HasSpineBroken = false;
+
+	UPROPERTY()
+	EMovementState MovementZombie = EMovementState::NORMAL;
 
 public:
 	// Sets default values for this character's properties
@@ -19,6 +55,8 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	void Gate(bool bOpen);
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
@@ -26,4 +64,31 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	void AnyDamage(int32 Damage, const FName& HitBoneName, class AActor* DamageCauser);
+	
+	void RenameBoneName();
+
+	bool ApplyDamageToBone();
+
+	void Dismemberment();
+
+	void ChangeMovementType();
+
+	void ApplyPhysics();
+
+	void SetCapsuleLocation();
+
+	void AddForceToBones();
+
+	UFUNCTION(BlueprintCallable)
+	FVector CalculateCrawlForce(float ForwardForce, float UpwardForce);
+	
+	UFUNCTION(BlueprintCallable)
+	FVector CalculateImpulse();
+
+	UFUNCTION(BlueprintCallable)
+	FVector GetBoneSocketLocation();
+
+	UFUNCTION(BlueprintCallable)
+	bool FindBrokenBones(TArray<FName> BoneNames);
 };
